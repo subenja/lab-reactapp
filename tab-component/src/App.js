273aec03fa56065e74f3fd40d1bb043562bf7e8d
subenja <42151421+subenja@users.noreply.github.com>
2020-01-18 10:54:58 -0500

    initial React App

---
+++ tab-component/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TabCompoent from './components/TabComponent.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TabCompoent />
+    </div>
+  );
+}
+
+export default App;
---
+++ tab-component/src/components/TabComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class TabComponent extends React.Component{
+    constructor(props) {
+        super(props) 
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+
+        return(
+            <div> 
+
+            </div>
+        )
+    }
+}
