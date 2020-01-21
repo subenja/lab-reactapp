47bb8e50c10ccbc2ee49534d7c523105a09361a3
subenja <42151421+subenja@users.noreply.github.com>
2020-01-21 15:18:49 -0500

    Login page

---
+++ antd-test/src/components/Login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+        <div style={{height:'500px', display:'flex', flexFlow:'row', justifyContent:'center', alignItems:'space-around'}}>
+<div style={{padding:'20px'}}><a href="#" class="fa fa-facebook"></a></div>
+
+<div style={{padding:'20px'}}>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+            <Button type="link" size={'large'}>
+                Signup
+            </Button>
+
+            <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+            </Button>
+        </Form.Item>
+      </Form>
+      </div>
+      </div>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
+export default WrappedNormalLoginForm
---
+++ antd-test/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'antd/dist/antd.css';
+
+import WrappedNormalLoginForm from './components/Login.js'
+
+ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById("root"));
