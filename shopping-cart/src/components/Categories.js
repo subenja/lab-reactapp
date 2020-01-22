778864d356c335f73b0974d7fe3e3fffc631d682
subenja <42151421+subenja@users.noreply.github.com>
2020-01-22 16:05:35 -0500

    render Menu

--- shopping-cart/src/components/Categories.js
+++ shopping-cart/src/components/Categories.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Menu } from "antd";
 
 export class Categories extends Component {
 
 
+  state = {
+    current: "1"
+  };
+
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
+
+  renderCategories() {
+    return this.props.categories.map( category =>
+      <Menu.Item key={category.id}><span>{category.name}</span></Menu.Item>
+      )
+  }
+
   render() {
     return (
       <div>
-        Categories
+        <Menu
+          onClick={this.handleClick}
+          defaultOpenKeys={["sub1"]}
+          selectedKeys={[this.state.current]}
+          mode="inline"
+        >
+        {this.renderCategories()}
+        </Menu>
       </div>
-    )
+    );
   }
 }
 
-export default Categories
+export default Categories;
